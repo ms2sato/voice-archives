36577a10d344cd206abe439262bd37424a4d334d
ms2sato <ms2.sato@gmail.com>
2020-04-29 03:51:17 +0900

    refactor webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,84 +1,78 @@
-//@see https://qiita.com/ovrmrw/items/d3d7ff119778f82c9672
-
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
-module.exports = function (env = {}) {
-  const cssModulesScopedName = '[path]___[name]__[local]___[hash:base64:5]'
-  const plugins = []
-  const moduleRules = []
+const cssModulesScopedName = '[path]___[name]__[local]___[hash:base64:5]'
 
-  ////////////////////////////////////// plugins
-  plugins.push(
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      filename: 'index.html'
-    }),
-    new CleanWebpackPlugin
-  )
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'public/index.html',
+    filename: 'index.html'
+  }),
+  new CleanWebpackPlugin
+]
 
-  ////////////////////////////////////// module.rules
-  moduleRules.push({
-    test: /\.tsx?$/,
-    exclude: [/node_modules/],
-    use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: [],
-          plugins: [
-            'transform-react-jsx',
-            ['react-css-modules', {
-              generateScopedName: cssModulesScopedName
-            }],
-          ]
-        }
-      },
-      'awesome-typescript-loader'
-    ]
-  }, {
-    test: /\.css$/,
-    use: ['style-loader',
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            localIdentName: cssModulesScopedName,  // @see https://github.com/rails/webpacker/issues/2197#issuecomment-517234086
-          },
-          importLoaders: 1,
-          sourceMap: true,
-        }
-      },
-      'postcss-loader',
-    ]
-  })
-
-  return {
-    mode: 'development',
-    context: process.cwd(),
-    entry: {
-      main: './src/index.tsx',
-      vendor: './config/vendor.ts',
-      polyfills: './config/polyfills.ts',
-    },
-    output: {
-      filename: 'static/js/[name].js',
-      path: path.join(__dirname, 'dist')
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-      rules: moduleRules,
-    },
-    plugins,
-    devtool: 'source-map',
-    performance: false,
-    optimization: {
-      splitChunks: {
-        name: 'vendor',
-        chunks: 'initial',
+const moduleRules = [{
+  test: /\.tsx?$/,
+  exclude: [/node_modules/],
+  use: [{
+      loader: 'babel-loader',
+      options: {
+        presets: [],
+        plugins: [
+          'transform-react-jsx',
+          ['react-css-modules', {
+            generateScopedName: cssModulesScopedName
+          }],
+        ]
       }
+    },
+    'awesome-typescript-loader'
+  ]
+}, {
+  test: /\.css$/,
+  use: ['style-loader',
+    {
+      loader: 'css-loader',
+      options: {
+        modules: {
+          localIdentName: cssModulesScopedName, // @see https://github.com/rails/webpacker/issues/2197#issuecomment-517234086
+        },
+        importLoaders: 1,
+        sourceMap: true,
+      }
+    },
+    'postcss-loader',
+  ]
+}]
+
+module.exports = {
+  mode: 'development',
+  context: process.cwd(),
+  entry: {
+    main: './src/index.tsx',
+    vendor: './config/vendor.ts',
+    polyfills: './config/polyfills.ts',
+  },
+  output: {
+    filename: 'static/js/[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: moduleRules,
+  },
+  plugins,
+  devtool: 'source-map',
+  performance: false,
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
     }
   }
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,80 +2,78 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
-module.exports = function (env = {}) {
-  const cssModulesScopedName = '[path]___[name]__[local]___[hash:base64:5]'
-  const plugins = []
-  const moduleRules = []
+const cssModulesScopedName = '[path]___[name]__[local]___[hash:base64:5]'
 
-  plugins.push(
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      filename: 'index.html'
-    }),
-    new CleanWebpackPlugin
-  )
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'public/index.html',
+    filename: 'index.html'
+  }),
+  new CleanWebpackPlugin
+]
 
-  plugins.push(
-    new CompressionWebpackPlugin({
-      test: /\.js$/
-    }),
-    new ExtractTextPlugin('static/css/bundle.[chunkhash].css')
-  )
+plugins.push(
+  new CompressionWebpackPlugin({
+    test: /\.js$/
+  }),
+  new ExtractTextPlugin('static/css/bundle.[chunkhash].css')
+)
 
-  moduleRules.push({
-    test: /\.tsx?$/,
-    exclude: [/node_modules/],
+const moduleRules = [{
+  test: /\.tsx?$/,
+  exclude: [/node_modules/],
+  use: [{
+      loader: 'babel-loader',
+      options: {
+        presets: [],
+        plugins: [
+          'transform-react-jsx',
+          ['react-css-modules', {
+            generateScopedName: cssModulesScopedName
+          }],
+        ]
+      }
+    },
+    'awesome-typescript-loader'
+  ]
+}, {
+  test: /\.css$/,
+  use: ExtractTextPlugin.extract({
+    fallback: 'style-loader',
     use: [{
-        loader: 'babel-loader',
+        loader: 'css-loader',
         options: {
-          presets: [],
-          plugins: [
-            'transform-react-jsx',
-            ['react-css-modules', {
-              generateScopedName: cssModulesScopedName
-            }],
-          ]
+          modules: {
+            localIdentName: cssModulesScopedName
+          },
+          importLoaders: 1,
+          sourceMap: true,
         }
       },
-      'awesome-typescript-loader'
+      'postcss-loader',
     ]
-  }, {
-    test: /\.css$/,
-    use: ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use: [{
-          loader: 'css-loader',
-          options: {
-            modules: {
-              localIdentName: cssModulesScopedName
-            },
-            importLoaders: 1,
-            sourceMap: true,
-          }
-        },
-        'postcss-loader',
-      ]
-    })
   })
+}]
 
-  return {
-    mode: 'production',
-    context: process.cwd(),
-    entry: ['./config/polyfills.ts', './src/index.tsx'],
-    output: {
-      filename: 'static/js/bundle.[chunkhash].js',
-      path: path.join(__dirname, 'dist')
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-    module: {
-      rules: moduleRules,
-    },
-    plugins,
-    devtool: 'source-map',
-    performance: false
-  }
+module.exports = {
+  mode: 'production',
+  context: process.cwd(),
+  entry: ['./config/polyfills.ts', './src/index.tsx'],
+  output: {
+    filename: 'static/js/bundle.[chunkhash].js',
+    path: path.join(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: moduleRules,
+  },
+  plugins,
+  devtool: 'source-map',
+  performance: false
 }
